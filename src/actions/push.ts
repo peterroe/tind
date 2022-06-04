e1a29b683211a86ee60eacdb90aa820048a6ffca
peterroe <peterroe@163.com>
2022-06-04 22:22:59 +0800

    feat:ignore lock file

--- src/actions/push.ts
+++ src/actions/push.ts
@@ -1,11 +1,13 @@
-import { spawn } from 'child_process'
-import { execa } from 'execa'
-import { dotTind } from '../utils'
+import { spawn } from "child_process";
+import { dotTind } from "../utils";
 
 export async function push() {
-  spawn('git add . && git commit -m "feat" && git pull origin master && git push origin master', {
-    stdio: 'inherit',
-    shell: true,
-    cwd: dotTind,
-  })
+  spawn(
+    'git add . && git commit -m "feat" && git pull origin master && git push origin master',
+    {
+      stdio: "inherit",
+      shell: true,
+      cwd: dotTind,
+    }
+  );
 }
