4db301ff4f30dc03be117b498f3dbea321c4b8c8
peterroe <peterroe@163.com>
2022-06-10 20:35:42 +0800

    refactor: change the way of login

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -5,16 +5,17 @@ import { dotTind, passWordJson, showAnimationLoading } from '../utils'
 const enquirer = new Enquirer()
 
 export async function login() {
-  const { repo } = await enquirer.prompt([
+  const { name } = await enquirer.prompt([
     {
       type: 'input',
-      name: 'repo',
-      message: 'What is your repo address?',
+      name: 'name',
+      message: 'What is your acount name?',
     },
   ])
+  const hub = `git@github.com:${name}/password.git`
   showAnimationLoading('clone repo to local...', 'login success!', async () => {
     const res = await fs.ensureDir(dotTind)
-    res && await execa('git', ['clone', repo, dotTind]) // clone to local if none
+    res && (await execa('git', ['clone', hub, dotTind])) // clone to local if none
     await fs.ensureFile(passWordJson)
   })
 }
