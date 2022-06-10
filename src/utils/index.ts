380b23936f98a84d5065fe219c6b92fad1244a37
peterroe <peterroe@163.com>
2022-06-10 20:24:04 +0800

    feat: code comment

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,26 +7,48 @@ import type { accountType } from '../types'
 export const home = os.homedir()
 
 export const dotTind = path.resolve(os.homedir(), '.tind')
-// /Users/bytedance/.tind
+// eg: /Users/bytedance/.tind
 
 export const passWordJson = path.resolve(dotTind, 'password.json')
-// /Users/bytedance/.tind/password.json
+// eg: /Users/bytedance/.tind/password.json
 
-export function showAnimationLoading(beginWord: string, endWord: string, callBack: Function) {
+/**
+ * @description: Add animation for time consuming task
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {Function} callBack
+ */
+export function showAnimationLoading(
+  beginWord: string,
+  endWord: string,
+  callBack: Function
+) {
   const spinner = ora(beginWord).start()
   callBack()
   spinner.succeed(endWord)
 }
 
+/**
+ * @description: Get content of password.json
+ * @return content
+ */
 export function getPassWordJson(): Promise<accountType[]> {
   // https://github.com/jprichardson/node-fs-extra/blob/master/docs/readJson-sync.md
   return fs.readJsonSync(passWordJson, { throws: false }) || []
 }
 
+/**
+ * @description: Update content of password.json
+ * @param {accountType} newAccounts new Content
+ */
 export function postPassWordJson(newAccounts: accountType[]) {
   return fs.writeJsonSync(passWordJson, newAccounts) || []
 }
 
+/**
+ * @description: Find the max id in a accountType array
+ * @param {accountType} accounts
+ */
 export function getMaxId(accounts: accountType[]): number {
   return accounts.reduce((maxId, account) => {
     return account.id > maxId ? account.id : maxId
